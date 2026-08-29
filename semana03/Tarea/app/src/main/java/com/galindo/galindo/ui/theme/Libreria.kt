5cf6bc0e3f7d5219fa5558071a7fc4b945a2b1bc
edugalindo-jpg <edu.galindo@tecsup.edu.pe>
2026-08-29 17:57:57 -0500

    se creo la estructura del codigo ingresando los datos por el codigo

---
+++ semana03/Tarea/app/src/main/java/com/galindo/galindo/ui/theme/Libreria.kt
@@ -0,0 +1,124 @@
+package com.galindo.galindo.ui.theme
+
+
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
+
+data class Prestamo(
+    val tituloLibro: String,
+    val tipoUsuario: String,
+    val fechaPrestamo: LocalDate,
+    val fechaDevolucion: LocalDate,
+    val fechaEntrega: LocalDate
+)
+
+fun calcularDiasRetraso(
+    fechaDevolucion: LocalDate,
+    fechaEntrega: LocalDate
+): Long {
+
+    return if (fechaEntrega.isAfter(fechaDevolucion)) {
+        ChronoUnit.DAYS.between(
+            fechaDevolucion,
+            fechaEntrega
+        )
+    } else {
+        0
+    }
+}
+
+fun calcularMulta(
+    tipoUsuario: String,
+    diasRetraso: Long
+): Double {
+
+    return when (tipoUsuario.lowercase()) {
+
+        "alumno" -> diasRetraso * 1.50
+
+        "maestro" -> diasRetraso * 3.00
+
+        else -> 0.0
+    }
+}
+
+fun mostrarResultado(prestamo: Prestamo) {
+
+    val diasRetraso = calcularDiasRetraso(
+        prestamo.fechaDevolucion,
+        prestamo.fechaEntrega
+    )
+
+    val multa = calcularMulta(
+        prestamo.tipoUsuario,
+        diasRetraso
+    )
+
+    println("=========================================")
+    println("             BIBLIOTECA")
+    println("=========================================")
+
+    println("Titulo del libro: ${prestamo.tituloLibro}")
+    println("Tipo de usuario: ${prestamo.tipoUsuario}")
+    println("Fecha de prestamo: ${prestamo.fechaPrestamo}")
+    println("Fecha de devolucion: ${prestamo.fechaDevolucion}")
+    println("Fecha de entrega: ${prestamo.fechaEntrega}")
+
+    if (diasRetraso > 0) {
+
+        println(
+            "Estado: Devuelto con $diasRetraso dia(s) de retraso"
+        )
+
+        println(
+            String.format("Multa: S/ %.2f", multa)
+        )
+
+    } else {
+
+        println("Estado: Devuelto a tiempo")
+        println("Multa: S/ 0.00")
+    }
+    println("-----------------------------------------")
+
+    println(
+        String.format("Total S/ %.2f deuda", multa)
+    )
+
+    println("=========================================")
+}
+
+fun main() {
+
+    val tituloLibro = "Programacion en Kotlin"
+
+    val tipoUsuario = "Alumno"
+
+    val fechaPrestamo = LocalDate.of(
+        2026,
+        8,
+        20
+    )
+
+    val fechaDevolucion = LocalDate.of(
+        2026,
+        8,
+        25
+    )
+
+    val fechaEntrega = LocalDate.of(
+        2026,
+        8,
+        28
+    )
+
+    val prestamo = Prestamo(
+        tituloLibro,
+        tipoUsuario,
+        fechaPrestamo,
+        fechaDevolucion,
+        fechaEntrega
+    )
+
+    mostrarResultado(prestamo)
+}
