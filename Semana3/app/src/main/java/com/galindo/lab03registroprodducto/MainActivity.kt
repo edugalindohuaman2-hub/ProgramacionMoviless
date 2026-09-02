9f20c4f496e3827bba4adb2eb182eda1d781624e
edugalindohuaman2-hub <edugalindohuaman2@gmail.com>
2026-09-02 17:21:51 -0500

    Agrega proyecto lab03 Semana03

---
+++ Semana3/app/src/main/java/com/galindo/lab03registroprodducto/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.galindo.lab03registroprodducto
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.galindo.lab03registroprodducto.ui.theme.Lab03RegistroProdductoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab03RegistroProdductoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab03RegistroProdductoTheme {
+        Greeting("Android")
+    }
+}
