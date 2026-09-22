7dd11fc89aced1370331326b548258081e7bf88d
edugalindo-jpg <edu.galindo@tecsup.edu.pe>
2026-09-21 23:22:23 -0500

    implementar grafo de navegacion

--- semana05/labaratorio/app/src/main/java/com/galindo/laboratorio/navigation/AppNavigation.kt
+++ semana05/labaratorio/app/src/main/java/com/galindo/laboratorio/navigation/AppNavigation.kt
@@ -1,2 +1,73 @@
 package com.galindo.laboratorio.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.galindo.laboratorio.screens.DetailScreen
+import com.galindo.laboratorio.screens.HomeScreen
+import com.galindo.laboratorio.screens.ListScreen
+import com.galindo.laboratorio.screens.ProfileScreen
+
+@Composable
+fun AppNavigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+
+        composable(
+            route = Screen.Home.route
+        ) {
+            HomeScreen(
+                onNavigate = { route ->
+                    navController.navigate(route)
+                }
+            )
+        }
+
+        composable(
+            route = Screen.List.route
+        ) {
+            ListScreen(
+                onNavigate = { route ->
+                    navController.navigate(route)
+                }
+            )
+        }
+
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument("itemId") {
+                    type = NavType.IntType
+                }
+            )
+        ) { backStackEntry ->
+
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+
+            DetailScreen(
+                itemId = itemId,
+                onBack = {
+                    navController.popBackStack()
+                }
+            )
+        }
+
+        composable(
+            route = Screen.Profile.route
+        ) {
+            ProfileScreen(
+                onBack = {
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
+}
