d0233a1c599f19e29b9a9d4988cc53de0c51dfbe
edugalindo-jpg <edu.galindo@tecsup.edu.pe>
2026-09-21 23:21:11 -0500

    crear pantalla de perfil

--- semana05/labaratorio/app/src/main/java/com/galindo/laboratorio/screens/ProfileScreen.kt
+++ semana05/labaratorio/app/src/main/java/com/galindo/laboratorio/screens/ProfileScreen.kt
@@ -1,2 +1,33 @@
 package com.galindo.laboratorio.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProfileScreen(
+    onBack: () -> Unit
+) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "Perfil del usuario"
+        )
+
+        Button(
+            onClick = onBack
+        ) {
+            Text("Regresar")
+        }
+    }
+}
