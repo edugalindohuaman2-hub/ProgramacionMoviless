cbef48421b56218334bc07e774c9aa633fb2ee9a
edugalindo-jpg <edu.galindo@tecsup.edu.pe>
2026-09-21 23:13:35 -0500

    crear rutas de navegacion

--- semana05/labaratorio/app/src/main/java/com/galindo/laboratorio/navigation/Screen.kt
+++ semana05/labaratorio/app/src/main/java/com/galindo/laboratorio/navigation/Screen.kt
@@ -1,4 +1,17 @@
 package com.galindo.laboratorio.navigation
 
-class Screen {
+sealed class Screen(val route: String) {
+
+    data object Home : Screen("home")
+
+    data object List : Screen("list")
+
+    data object Detail : Screen("detail/{itemId}") {
+
+        fun createRoute(itemId: Int): String {
+            return "detail/$itemId"
+        }
+    }
+
+    data object Profile : Screen("profile")
 }
