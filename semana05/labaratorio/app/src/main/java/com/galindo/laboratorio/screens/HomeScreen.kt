6ef9cc42ccb959f2a78022f97da60cf6366e9984
edugalindo-jpg <edu.galindo@tecsup.edu.pe>
2026-09-21 23:16:13 -0500

    crear pantalla principal

--- semana05/labaratorio/app/src/main/java/com/galindo/laboratorio/screens/HomeScreen.kt
+++ semana05/labaratorio/app/src/main/java/com/galindo/laboratorio/screens/HomeScreen.kt
@@ -1,2 +1,42 @@
 package com.galindo.laboratorio.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.galindo.laboratorio.navigation.Screen
+@Composable
+fun HomeScreen(
+    onNavigate: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "Pantalla Principal"
+        )
+
+        Button(
+            onClick = {
+                onNavigate(Screen.List.route)
+            }
+        ) {
+            Text("Ver lista")
+        }
+
+        Button(
+            onClick = {
+                onNavigate(Screen.Profile.route)
+            }
+        ) {
+            Text("Ver perfil")
+        }
+    }
+}
